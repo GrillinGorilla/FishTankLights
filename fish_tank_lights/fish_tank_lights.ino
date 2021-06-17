57c2ced4cc4e15747c64362cd5dd1205bb1a7c61
Dave <schaefd2@gmail.com>
2021-06-17 12:04:09 -0400

    latest

--- fish_tank_lights/fish_tank_lights.ino
+++ fish_tank_lights/fish_tank_lights.ino
@@ -138,7 +138,7 @@ void setup() {
   // End of trinket special code
 
   strip.begin();
-  strip.setBrightness(100);
+  strip.setBrightness(50);
   strip.show(); // Initialize all pixels to 'off'
 }
 
@@ -147,36 +147,109 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  entireStrandWheel(1);
   set_entire_strip_color(1, PURE_WHITE);
-  //set_single_pixel(0);
   rainbow_chase(100);
   rainbow_chase(100);
   rainbow_chase(100);
   rainbow_chase(100);
   rainbow_chase(100);
+  entireStrandWheel(1);
   set_entire_strip_color(1, GREEN);
   rainbow_chase(100);
   rainbow_chase(100);
   rainbow_chase(100);
   rainbow_chase(100);
   rainbow_chase(100);
+  entireStrandWheel(1);
   set_entire_strip_color(1, BLUE);
   rainbow_chase(100);
   rainbow_chase(100);
   rainbow_chase(100);
   rainbow_chase(100);
   rainbow_chase(100);
-  set_entire_strip_color(1, RED);
-  //bright_sunny_day_no_clouds();
+  entireStrandWheel(1);
+  set_entire_strip_color(1, GREENISH_WHITE);
+
 }
 
 
 //###########################################################
 // Support functions
 
+void entireStrandWheel(uint8_t wait) {
+  for (int j=0; j < 256; j++) {
+    strip.fill(Wheel(j));
+    
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
 void bright_sunny_day_no_clouds()
 {
-  strip.setBrightness(200);
+  strip.setBrightness(50);
   strip.fill(RGB_WHITE, 0, NUM_OF_PIXELS);
   strip.show();
 }
